9a3d7486b073297bcb07bbe2ca3e44f44b47a795
caokunyu <cky9302@163.com>
2021-06-30 22:30:31 +0800

    Promise 方法

---
+++ loadsh/12.promise.js
@@ -0,0 +1,44 @@
+Promise.any = function(ps) {
+  let r, j;
+  const promise = new Promise((resolve, reject) => {
+    r = resolve;
+    j = reject;
+  });
+  let errCount = 0;
+  let pCount = 0;
+  for (const p of ps) {
+    pCount += 1;
+    Promise.resolve(p).then(
+      val => r(val),
+      err => {
+        errCount += 1;
+        if (errCount >= pCount) {
+          j(err);
+        }
+      }
+    );
+  }
+  return promise;
+};
+
+Promise.all = function(promises) {
+  return new Promise(function(resolve, reject) {
+    if (!isArray(promises)) {
+      return reject(new TypeError('arguments must be an array'));
+    }
+    var resolvedCounter = 0;
+    var promiseNum = promises.length;
+    var resolvedValues = new Array(promiseNum);
+    for (let i = 0; i < promiseNum; i++) {
+        Promise.resolve(promises[i]).then(function(value) {
+          resolvedCounter++
+          resolvedValues[i] = value
+          if (resolvedCounter == promiseNum) {
+            return resolve(resolvedValues)
+          }
+        }, function(reason) {
+          return reject(reason)
+        })
+    }
+  })
+}
