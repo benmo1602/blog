69a605b37dc3c12d2cb1a91d92c5733ae1a367a0
caokunyu <cky9302@163.com>
2022-01-17 10:30:26 +0800

    feat: shell 测试

---
+++ shell/hellow.sh
@@ -0,0 +1,10 @@
+###
+ # @Description: description
+ # @Author: kunyu.cao@hlifetech.com
+ # @Date: 2022-01-12 15:42:16
+ # @LastEditors: kunyu.cao@hlifetech.com
+ # @LastEditTime: 2022-01-12 15:42:17
+###
+your_name="qinjx"
+echo $your_name
+echo ${your_name}
